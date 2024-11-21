6e047914ea40c56aaae1a7200ca8bc2d7713290f
Joel <simkalok0914@gmail.com>
2024-11-21 20:24:54 +0800

    Fixed build issue
    Fixed permission issue

--- Assets/Scripts/QRScanner.cs
+++ Assets/Scripts/QRScanner.cs
@@ -1,8 +1,10 @@
-using System;
-using TMPro;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 using ZXing;
+#if UNITY_ANDROID
+using UnityEngine.Android;
+#endif
 
 public class QRScanner : MonoBehaviour
 {
@@ -16,24 +18,82 @@ public class QRScanner : MonoBehaviour
     public WebCamTexture cameraTexture;
     public bool scanning;
 
+    [Header("Config")]
+    public string logPrefix = "[QRScanner] ";
+
     public void Start()
     {
+#if UNITY_IOS || UNITY_WEBGL
+        StartCoroutine(AskForPermissionIfRequired(UserAuthorization.WebCam, () => { SetUpCamera(); }));
+        return;
+#elif UNITY_ANDROID
+        if (!Permission.HasUserAuthorizedPermission(Permission.Camera))
+        {
+            AskCameraPermission();
+            return;
+        }
+#endif
         SetUpCamera();
     }
     public void Update()
     {
         UpdateCameraRender();
-        if (!scanning)
+        Scan();
+    }
+
+#if UNITY_IOS || UNITY_WEBGL
+    private bool CheckPermissionAndRaiseCallbackIfGranted(UserAuthorization authenticationType, Action authenticationGrantedAction)
+    {
+        if (Application.HasUserAuthorization(authenticationType))
         {
-            scanning = true;
-            Scan();
+            if (authenticationGrantedAction != null)
+                authenticationGrantedAction();
+
+            return true;
+        }
+        return false;
+    }
+
+    private IEnumerator AskForPermissionIfRequired(UserAuthorization authenticationType, Action authenticationGrantedAction)
+    {
+        if (!CheckPermissionAndRaiseCallbackIfGranted(authenticationType, authenticationGrantedAction))
+        {
+            yield return Application.RequestUserAuthorization(authenticationType);
+            if (!CheckPermissionAndRaiseCallbackIfGranted(authenticationType, authenticationGrantedAction))
+                Debug.LogWarning($"{logPrefix}Permission {authenticationType} Denied");
         }
     }
+#elif UNITY_ANDROID
+    private void PermissionCallbacksPermissionGranted(string permissionName)
+    {
+        StartCoroutine(DelayedCameraInitialization());
+    }
+
+    private IEnumerator DelayedCameraInitialization()
+    {
+        yield return null;
+        SetUpCamera();
+    }
+
+    private void PermissionCallbacksPermissionDenied(string permissionName)
+    {
+        Debug.LogWarning($"{logPrefix}Permission {permissionName} Denied");
+    }
+
+    private void AskCameraPermission()
+    {
+        var callbacks = new PermissionCallbacks();
+        callbacks.PermissionDenied += PermissionCallbacksPermissionDenied;
+        callbacks.PermissionGranted += PermissionCallbacksPermissionGranted;
+        Permission.RequestUserPermission(Permission.Camera, callbacks);
+    }
+#endif
 
     void SetUpCamera()
     {
         WebCamDevice[] devices = WebCamTexture.devices;
 
+        Debug.Log(logPrefix + "WebCam Devices: " + devices.Length);
         if (devices.Length == 0)
         {
             isCamAvailable = false;
@@ -48,9 +108,16 @@ public class QRScanner : MonoBehaviour
             }
         }
 
-        cameraTexture.Play();
-        backgroundRawImage.texture = cameraTexture;
-        isCamAvailable = true;
+        if (cameraTexture != null)
+        {
+            cameraTexture.Play();
+            backgroundRawImage.texture = cameraTexture;
+            isCamAvailable = true;
+        }
+        else
+        {
+            isCamAvailable = false;
+        }
     }
 
     void UpdateCameraRender()
@@ -60,22 +127,20 @@ public class QRScanner : MonoBehaviour
         float ratio = (float)cameraTexture.width / (float)cameraTexture.height;
         aspectRatioFitter.aspectRatio = ratio;
 
-        int orientation = cameraTexture.videoRotationAngle;
+        int orientation = -cameraTexture.videoRotationAngle;
         backgroundRawImage.rectTransform.localEulerAngles = new Vector3(0, 0, orientation);
     }
 
     void Scan()
     {
+        if (!isCamAvailable) return;
+
         try
         {
             IBarcodeReader barcodeReader = new BarcodeReader();
             Result result = barcodeReader.Decode(cameraTexture.GetPixels32(), cameraTexture.width, cameraTexture.height);
 
-            if (result == null)
-            {
-                Scan();
-            }
-            else
+            if (result != null)
             {
                 // TODO: after getting result
                 Application.OpenURL(result.Text);
@@ -83,7 +148,6 @@ public class QRScanner : MonoBehaviour
         }
         catch
         {
-            Scan();
         }
     }
 }
